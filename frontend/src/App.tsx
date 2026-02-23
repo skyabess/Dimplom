f0af2d1ce293a88910441dc03f0ef119cdc3eda7
skyabess <pozzigames459@gmail.com>
2026-02-23 15:05:44 +0300

    создание index.html

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Land Contracts Frontend</h1>
+        <p>Система управления земельными контрактами</p>
+      </header>
+      <main>
+        <p>Добро пожаловать в систему управления земельными контрактами!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
